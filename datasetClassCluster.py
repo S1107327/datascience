580432ac1958d5c255430b9223c8c4d82375ab6e
lorti <s1107327@studenti.univpm.it>
2023-04-05 18:12:58 +0200

    Start

---
+++ datasetClassCluster.py
@@ -0,0 +1,30 @@
+import pandas as pd
+legit = []
+with open("legit/5000.txt", "r") as fin:
+    line = fin.readline().strip('\n')
+    while line:
+        legit.append(line)
+        line = fin.readline().strip('\n')
+
+labels = ['legit' for i in range(5000)]
+
+legit_df = pd.DataFrame({'label': pd.Series(labels), 'domain': pd.Series(legit)})
+
+# dga part
+dga = []
+labels_dga = ['dga' for i in range(5000)]
+
+for family in ["cryptolocker", "dircrypt", "dyre", "padcrypt", "zeus-newgoz"]:
+    with open(f"{family}/list/1000.txt", "r") as fin:
+        line = fin.readline().strip('\n')
+        while line:
+            dga.append(line)
+            line = fin.readline().strip('\n')
+
+dga_df = pd.DataFrame({'label': pd.Series(labels_dga), 'domain': pd.Series(dga)})
+complete = pd.concat([legit_df, dga_df]).reset_index(drop=True)
+no_dots = [x.replace(".", "") for x in complete["domain"]]
+bigrams_column = [" ".join([x[i:i+2] for i in range(0, len(x)-1, 1)]) for x in no_dots]
+complete['no_dots'] = pd.Series(no_dots)
+complete['bigrams'] = pd.Series(bigrams_column)
+complete.to_csv('example_dataset.csv', index=False)
