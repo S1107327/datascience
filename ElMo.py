434c943346c5214b8a667dc1cf8c0d3623526c87
lorti <s1107327@studenti.univpm.it>
2023-04-17 18:22:42 +0200

    ElMo e classificazione

---
+++ ElMo.py
@@ -0,0 +1,17 @@
+# import necessary libraries
+import tensorflow_hub as hub
+import tensorflow.compat.v1 as tf
+import pandas as pd
+import numpy as np
+
+tf.disable_eager_execution()
+dataset = pd.read_csv("example_dataset.csv")
+# Load pre trained ELMo model
+elmo = hub.Module("ELMo")
+embedded = elmo(dataset['no_dots'], signature="default", as_dict=True)
+init = tf.global_variables_initializer()
+sess = tf.Session()
+sess.run(init)
+embeddings = sess.run(embedded['default'])
+print(embeddings)
+print((len(embeddings[0])))
