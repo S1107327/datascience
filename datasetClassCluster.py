c279ab8a345ec75a87b12c29ac89b57fd5764aad
lorenzo <s1107327@studenti.univpm.it>
2023-04-19 19:08:43 +0200

    Continuazione class cluster

--- datasetClassCluster.py
+++ datasetClassCluster.py
@@ -1,18 +1,18 @@
 import pandas as pd
 legit = []
-with open("legit/5000.txt", "r") as fin:
+with open("legit/list/5000.txt", "r") as fin:
     line = fin.readline().strip('\n')
     while line:
         legit.append(line)
         line = fin.readline().strip('\n')
 
-labels = ['legit' for i in range(5000)]
+labels = [0 for i in range(5000)]
 
 legit_df = pd.DataFrame({'label': pd.Series(labels), 'domain': pd.Series(legit)})
 
 # dga part
 dga = []
-labels_dga = ['dga' for i in range(5000)]
+labels_dga = [1 for i in range(5000)]
 
 for family in ["cryptolocker", "dircrypt", "dyre", "padcrypt", "zeus-newgoz"]:
     with open(f"{family}/list/1000.txt", "r") as fin:
@@ -27,7 +27,7 @@ no_dots = [x.replace(".", "") for x in complete["domain"]]
 bigrams_column = [" ".join([x[i:i+2] for i in range(0, len(x)-1, 1)]) for x in no_dots]
 complete['no_dots'] = pd.Series(no_dots)
 complete['bigrams'] = pd.Series(bigrams_column)
-#complete.to_csv('example_dataset.csv', index=False)
+complete.to_csv('example_dataset.csv', index=False)
 with open("bigrams.txt", 'w') as fout:
     for name in complete['bigrams']:
         fout.write(f"{name}\n")
