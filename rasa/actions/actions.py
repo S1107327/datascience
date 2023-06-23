5b9a2fed9dd9021c714772ac22ed33e66ba69c1b
lorenzo <s1107327@studenti.univpm.it>
2023-06-23 17:32:36 +0200

    Agganciato il bot a telegram e scritto primo intent e action

--- rasa/actions/actions.py
+++ rasa/actions/actions.py
@@ -4,7 +4,6 @@
 # See this guide on how to implement these action:
 # https://rasa.com/docs/rasa/custom-actions
 
-
 # This is a simple example for a custom action which utters "Hello World!"
 
 # from typing import Any, Text, Dict, List
@@ -25,3 +24,54 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+import json
+import sys
+sys.path.append("../")
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from typing import Any, Text, Dict, List
+from mongodb.mongo_utils import MongoDBConnector
+
+class ActionShowRestaurant(Action):
+    def name(self) -> Text:
+        return "action_restaurant_list"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        mongo_db = MongoDBConnector()
+        restaurant_json = mongo_db.get_restaurant_list()
+        mydict = dict()
+        i = 1
+        for restaurant in restaurant_json:
+            address_dict = {"building": restaurant["address"]["building"],
+                            "coord:": f"lat: {restaurant['address']['coord'][0]} lon: {restaurant['address']['coord'][1]}",
+                            "street": restaurant["address"]["street"],
+                            "zipcode": restaurant["address"]["zipcode"]}
+            mydict[i] = {"name": restaurant['name'],
+                           "borough": restaurant['borough'],
+                           "address": f"{address_dict['street']}, {address_dict['zipcode']}, in building {address_dict['building']}",
+                           "cuisine": restaurant["cuisine"]}
+            i += 1
+        restaurants = json.dumps(mydict, indent=2, sort_keys=True)
+        dispatcher.utter_message(text=restaurants)
+
+        return []
+
+class ActionReserveTable(Action):
+    def name(self) -> Text:
+        return "action_reserve_table"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        pass
+
+class ActionReviewRestaurant(Action):
+    def name(self) -> Text:
+        return "action_review_restaurant"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        pass
