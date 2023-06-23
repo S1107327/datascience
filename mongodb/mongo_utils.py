252020b1c58f179a1f8060d67b04a0271d78aef2
lorenzo <s1107327@studenti.univpm.it>
2023-06-23 17:37:01 +0200

    Agganciato il bot a telegram e scritto primo intent e action

---
+++ mongodb/mongo_utils.py
@@ -0,0 +1,13 @@
+from pymongo import MongoClient
+
+class MongoDBConnector:
+    def __init__(self):
+        # Provide the mongodb atlas url to connect python to mongodb using pymongo
+        self.CONNECTION_STRING = "localhost:27017"
+        # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
+        self.client = MongoClient(self.CONNECTION_STRING)
+
+    def get_restaurant_list(self):
+        restaurants = self.client['rasa_db']['restaurants'].find().limit(5)
+        return restaurants
+
