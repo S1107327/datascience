ad2282cf9b6cc17165f257269fdb35f62f6cbdc5
lorenzo <s1107327@studenti.univpm.it>
2023-06-27 18:17:20 +0200

    push provvisorio

--- mongodb/mongo_utils.py
+++ mongodb/mongo_utils.py
@@ -7,7 +7,10 @@ class MongoDBConnector:
         # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
         self.client = MongoClient(self.CONNECTION_STRING)
 
-    def get_restaurant_list(self):
-        restaurants = self.client['rasa_db']['restaurants'].find().limit(5)
+    def get_restaurant_names_list(self):
+        restaurants = self.client['rasa_db']['restaurants'].find({}, {"name": 1})
         return restaurants
 
+    def get_restaurant_info(self, name):
+        restaurant_info = self.client['rasa_db']['restaurants'].find({"name": name}).distinct("name")
+        return restaurant_info
--- rasa/actions/actions.py
+++ rasa/actions/actions.py
@@ -27,10 +27,12 @@
 import json
 import sys
 sys.path.append("../")
-from rasa_sdk import Action, Tracker
+from rasa_sdk import Action, Tracker, FormValidationAction
 from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.types import DomainDict
 from typing import Any, Text, Dict, List
 from mongodb.mongo_utils import MongoDBConnector
+
 
 class ActionShowRestaurant(Action):
     def name(self) -> Text:
@@ -40,23 +42,63 @@ class ActionShowRestaurant(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         mongo_db = MongoDBConnector()
-        restaurant_json = mongo_db.get_restaurant_list()
-        mydict = dict()
-        i = 1
+        restaurant_json = mongo_db.get_restaurant_names_list()
+        text_to_display = ""
         for restaurant in restaurant_json:
-            address_dict = {"building": restaurant["address"]["building"],
-                            "coord:": f"lat: {restaurant['address']['coord'][0]} lon: {restaurant['address']['coord'][1]}",
-                            "street": restaurant["address"]["street"],
-                            "zipcode": restaurant["address"]["zipcode"]}
-            mydict[i] = {"name": restaurant['name'],
-                           "borough": restaurant['borough'],
-                           "address": f"{address_dict['street']}, {address_dict['zipcode']}, in building {address_dict['building']}",
-                           "cuisine": restaurant["cuisine"]}
-            i += 1
-        restaurants = json.dumps(mydict, indent=2, sort_keys=True)
-        dispatcher.utter_message(text=restaurants)
+            text_to_display += f"{restaurant['name']}\n"
+        dispatcher.utter_message(text=text_to_display,
+                                 buttons=[{"title": "MORE", "payload": "/restaurant_info"}])
 
         return []
+
+
+class ValidateRestaurantInfoForm(FormValidationAction):
+    def name(self) -> Text:
+        return "validate_info_restaurant_form"
+
+    def validate_restaurant_name(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        mongo_db = MongoDBConnector()
+        restaurants = mongo_db.get_restaurant_names_list()
+        restaurant_name_list = []
+        for restaurant in restaurants:
+            restaurant_name_list.append(str(restaurant['name']).lower())
+        if slot_value.lower() not in restaurant_name_list:
+            dispatcher.utter_message(text=f"Sorry but the restaurant {slot_value} is not registered on the service")
+            return {"restaurant_name": None}
+        dispatcher.utter_message(text=f"OK! I'll show you more info about {slot_value} restaurant.")
+        return {"restaurant_name": slot_value}
+
+
+class ActionInfoRestaurant(Action):
+    def name(self) -> Text:
+        return "action_info_restaurant"
+
+    def run(self, dispatcher: "CollectingDispatcher",
+        tracker: Tracker,
+        domain: "Dict[Text, Any]",) -> List[Dict[Text, Any]]:
+        mongo_db = MongoDBConnector()
+        restaurant_name = tracker.get_slot('restaurant_name')
+        info_restaurant = mongo_db.get_restaurant_info(restaurant_name)
+        address_dict = {"building": info_restaurant["address"]["building"],
+                        "lon": f"{info_restaurant['address']['coord'][0]}",
+                        "lat:": f"lat: {info_restaurant['address']['coord'][1]}",
+                        "street": info_restaurant["address"]["street"],
+                        "zipcode": info_restaurant["address"]["zipcode"]}
+        mydict = {"name": info_restaurant['name'],
+                     "borough": info_restaurant['borough'],
+                     "address": f"{address_dict['street']}, {address_dict['zipcode']}, in building {address_dict['building']}",
+                     "cuisine": info_restaurant["cuisine"]}
+        text_to_display = ""
+        for k,v in mydict.items():
+            text_to_display += f"{k}: {v}\n"
+        dispatcher.utter_message(text=text_to_display,
+                                 attachment=f"https://www.google.com/maps/search/?api=1&query={address_dict['lat']}%2C{address_dict['lon']}")
 
 class ActionReserveTable(Action):
     def name(self) -> Text:
