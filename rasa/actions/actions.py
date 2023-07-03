c4a7fdbca0fae613fb4874cd7c8897932f91aad0
Lorenzo S <lorenzosopranzetti99@gmail.com>
2023-07-03 18:46:56 +0200

    Validazione form prenotazione e prima story per unhappy path

--- rasa/actions/actions.py
+++ rasa/actions/actions.py
@@ -1,11 +1,15 @@
+from re import template
 import sys
+from threading import current_thread
 sys.path.append("../")
 from rasa_sdk import Action, Tracker, FormValidationAction
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.types import DomainDict
 from typing import Any, Text, Dict, List
 from mongodb.mongo_utils import MongoDBConnector
-from rasa_sdk.events import SlotSet
+from rasa_sdk.events import SlotSet, AllSlotsReset
+from datetime import datetime
+
 
 ### START ACTIONS ###
 class ActionStart(Action):
@@ -149,8 +153,7 @@ class ActionReserveTable(Action):
         reservation['name'] = tracker.get_slot("name")
         reservation['phone_number'] = tracker.get_slot("phone_number")
         mongo_db.save_reservation(name, reservation)
-        #TODO: Inserire messaggio conferma prenotazione
-        return [SlotSet("restaurant_name_reservation",None)]
+        return [AllSlotsReset()]
 
 class ActionShowReservations(Action):
     def name(self) -> Text:
@@ -172,6 +175,7 @@ class ActionShowReservations(Action):
             else:
                 text_to_display += f"{reservation['name']} for {reservation['people_number']} person {reservation['date']} at {reservation['time']}\n Name: {reservation['name']}\n"
         dispatcher.utter_message(text=text_to_display)
+
 class ActionReviewRestaurant(Action):
     def name(self) -> Text:
         return "action_review_restaurant"
@@ -180,4 +184,119 @@ class ActionReviewRestaurant(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         pass
+
+class ActionClearFormSlots(Action):
+    def name(self) -> Text:
+        return "action_clear_form_slots"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(text="Form slot cleared")
+        return[AllSlotsReset()]
+
+
+class ActionShowReservationInfo(Action):
+    def name(self) -> Text:
+        return "action_show_reservation_info"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        utter_text = "This are your reservation infos:\n"
+        utter_text += f"Restaurant: {tracker.get_slot('restaurant_name_reservation')}\n"
+        utter_text += f"Number of people: {tracker.get_slot('people_number')}\n"
+        utter_text += f"Date: {tracker.get_slot('date')}\n"
+        utter_text += f"Reservation time: {tracker.get_slot('time')}\n"
+        utter_text += f"Reservation name: {tracker.get_slot('name')}\n"
+        utter_text += f"Given phone number: {tracker.get_slot('phone_number')}"
+        dispatcher.utter_message(text=utter_text)
+
+class ValidateReservationForm(FormValidationAction):
+    def name(self) -> Text:
+        return "validate_reservation_form"
+
+    def validate_restaurant_name_reservation(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        mongo_db = MongoDBConnector()
+        restaurants = mongo_db.get_restaurant_names_list()
+        restaurant_name_list = []
+        for restaurant in restaurants:
+            restaurant_name_list.append(str(restaurant['name']).lower())
+        if slot_value.lower() not in restaurant_name_list:
+            dispatcher.utter_message(text=f"Sorry but the restaurant {slot_value} is not registered on the service.")
+            return {"restaurant_name_reservation": None}
+        return {"restaurant_name_reservation": slot_value}
+
+    #TODO: validare anche se il numero di posti disponibili nel db è sufficiente, ancora non fatto lo script
+    def validate_people_number(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        mongo_db = MongoDBConnector()
+        if not slot_value.isdigit():
+            dispatcher.utter_message(text=f"Sorry but specified people number is not correct or valid.")
+            return {"people_number": None}
+        return {"people_number": slot_value}
+
+    def validate_phone_number(
+        self,
+        slot_value: Text,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        if not len(slot_value) == 10:
+            dispatcher.utter_message(text=f"Sorry but specified phone number is not valid! Retry.")
+            return {"phone_number": None}
+        return {"phone_number": slot_value}
+
+    def validate_date(
+        self,
+        slot_value: Text,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        current_date = datetime.now().date()
+        date_format = ["%Y-%m-%d", "%Y%m%d", "%Y/%m/%d", "%d-%m-%Y", "%d/%m/%Y"]
+        date_is_correct = False
+        for format in date_format:
+            try:
+                formatted_date = datetime.strptime(slot_value, format).date()
+                if formatted_date < current_date:
+                    dispatcher.utter_message(text="Sorry, reservation date can't be a past date.")
+                    return {"date":None}
+                else:
+                    formatted_date = formatted_date.strftime('%d-%m-%Y')
+                    return {"date": formatted_date}
+            except Exception:
+                pass
+        dispatcher.utter_message(text="Inserted date is not correct.")
+        return {'date':None}
+
+    def validate_time(
+        self,
+        slot_value: Text,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        time_format = "%H:%M"
+        try:
+            formatted_time = datetime.strptime(slot_value, time_format)
+            return {'time':slot_value}
+        except Exception:
+            pass
+        dispatcher.utter_message(text="Inserted time is not correct. Retry!")
+        return {'time':None}
+
 
